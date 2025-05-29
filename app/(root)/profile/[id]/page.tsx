e041996df35c71e36b9c42bcb61b53fcd40a86ae
Veronika Fedorova <fedorovavv03@gmail.com>
2025-05-29 18:16:59 +0300

    Implemented Auth UI

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,18 +1,18 @@
-import Header from "@/components/Header";
-import VideoCard from "@/components/VideoCard";
-import { dummyCards } from "@/constants";
+import Header from '@/components/Header';
+import VideoCard from '@/components/VideoCard';
+import { dummyCards } from '@/constants';
 
 const Page = async ({ params }: ParamsWithSearch) => {
   const { id } = await params;
   return (
-    <div className="wrapper page">
+    <div className='wrapper page'>
       <Header
-        title="Veronika"
-        subHeader="test@test.test"
-        userImg="/assets/images/dummy.jpg"
+        title='Veronika'
+        subHeader='test@test.test'
+        userImg='/assets/images/dummy.jpg'
       />
       User id: {id}
-      <section className="video-grid">
+      <section className='video-grid'>
         {dummyCards.map((card) => (
           <VideoCard {...card} key={card.id} />
         ))}
